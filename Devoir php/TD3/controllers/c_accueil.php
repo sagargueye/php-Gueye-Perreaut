155d78a2ecd4d2caf8aaa4fbe768a24d85f3f83a
codtest <f.perreaut69@gmail.com>
2018-02-27 20:40:50 +0100

    Add files via upload

--- Devoir php/TD3/controllers/c_accueil.php
+++ Devoir php/TD3/controllers/c_accueil.php
@@ -3,11 +3,58 @@
 require_once(PATH_MODELS.'filmDAO.php');
 require_once(PATH_MODELS.'genreDAO.php');
 
+
+
+if(isset($_GET['idi']))
+{
+
+$idi=htmlspecialchars($_GET['idi']); // variable donnée avec un GET
+$fDAO=new filmDAO();
+
+$i=1;
+$gd=0;
+
+// boucle qui permet de savoir si la variable idi correspond ou non à un film dans la base de donnéés
+do
+{ 
+	$f=$fDAO->getid($i);
+    if($f!=null)
+    {
+        if($f->getid()==$idi)
+        {
+            $gd=1; // idi correspond bien à un film
+        }
+	$i=$i+1;
+    }
+}
+while($f!=null);
+
+if($gd==1) 
+{
+$f=$fDAO->getid($idi); // récupération des informations du film avec cette ID
+
+$image=$f->getnomfichier(); // 
+$resu=$f->getresume();      // récupératon individuel de toutes ses informations
+$titre=$f->gettitre();      //
+
+$gDAO=new genreDAO(); 
+$g=$gDAO->getid($f->getgenid()); // récupération des information du genre du film
+$genre=$g->getlibelle();        // récupération du libelle du film
+}
+else
+{
+    $page="404";
+}
+
+
+
+}
+else
+{
+
 $ge=array(); //tableau qui contient la liste de tous les genres de la table genre
 $aid= array(); // tableau qui contient la liste des films en fonction du genre sélectionné dans la combobox
 $cpt=0;  // compte les film affiché sur la page accueil
-
-
 
 $gDAO=new genreDAO();
 
@@ -151,6 +198,8 @@ else
 	while($f!=null);
 
 	
+}
+
 }
 
 }
--- Devoir php/TD3/views/v_accueil.php
+++ Devoir php/TD3/views/v_accueil.php
@@ -21,8 +21,54 @@
 
 <!--  Début de la page -->
 
+<?php if(isset($_GET['idi']))
+{
+?>
+
+<h1>Les détails du film</h1>
+
+<!--  Affichage du l'image du film et du tableau contenant toutes ses informations  -->
+
+	 <div class = "col-md-6 col-sm-6 col-xs-12"> 
+	<img
+         src="<?= PATH_IMAGES.$image?>"
+        alt="Affiche du film"
+        height="800px" 
+        width="500px" 
+        />
+	</div>
+	<div class = "col-md-6 col-sm-6 col-xs-12"> 
+	<table class="table table-bordered"  > 
+	    <tr>
+			<td>Titre</td>
+			<td><?=$titre?></td>
+		</tr>
+		<tr>
+			<td>Résumé</td>
+			<td><?=$resu?></td>
+		</tr>
+		<tr>
+			<td>Nom du fichier</td>
+			<td><?=$image?></td>
+		</tr>
+		<tr>
+			<td>Genre</td>
+			<td><a href="index.php?genre=<?=$genre?>"><?=$genre?></a></td>
+		</tr>
+	</table>
+	</div>
+
+
+<?php
+} 
+else
+{
+?>	
+
+
+
 <!--Formulaire permmetant d'afficher les éléments de la combobox en fonction des informations stocker dans la table genre-->
-<form action="index.php?page=accueil" method="post"> 
+<form action="index.php" method="post"> 
  <label>Quels films souhaitez-vous afficher ?  <select name="liste">
   <option value="Tous les genres" selected>Tous les genres</option>
   <?php foreach  ($ge as $val) { ?>
@@ -53,7 +99,7 @@ if($cpt==0) // aucun film n'est compté
 <!--Boucle permmetant d'afficher toutes les images des film en fonction du genre sélectionné dans la combobox-->
 <?php foreach($aid as $val)
 
-{ ?><a href="index.php?page=hello&idi=<?=$val->getid()?>"/>
+{ ?><a href="index.php?idi=<?=$val->getid()?>"/>
      <img
          src="<?= PATH_IMAGES.$val->getnomfichier()?>"
     alt="Affiche du film"
@@ -62,8 +108,10 @@ if($cpt==0) // aucun film n'est compté
     /> 
 	</a>
 <?php 
+} 
 
-} ?>
+}
+?>
 
 
 <!--  Fin de la page -->
